e5bf14cfbf7e6352fcc0d1a3ce59508e93e1da5c
NaufalOpalMcpe <32413394+NaufalOpalMcpe@users.noreply.github.com>
2017-10-09 20:57:35 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ class LINE extends Command {
 
 
     get myBot() {
-        const bot = ['u3b257ce1497b8d24ec3708ba3ed79d46','u236b88bf1eac2b90e848a6198152e647','u763977dab29cbd6fa0cbfa9f159b768b'];
+        const bot = ['u3b257ce1497b8d24ec3708ba3ed79d46','u236b88bf1eac2b90e848a6198152e647','u763977dab29cbd6fa0cbfa9f159b768b','uc8e2c2b906e2322592c6d8f91a0957f7'];
         return bot; 
     }
 
